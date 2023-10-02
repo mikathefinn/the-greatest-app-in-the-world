023a9459d8fdbf678a2708122e467c4039f75ae9
jronimus <joni.ronimus@gmail.com>
2023-10-02 09:44:36 +0300

    translation function working, but translation not finished

--- scripts/changelanguage.js
+++ scripts/changelanguage.js
@@ -10,9 +10,14 @@ function loadTranslations() {
     .then((response) => response.json())
     .then((data) => {
       // Update HTML elements with translated content
-      const usagetextElement = document.getElementById("usagetext");
-      usagetextElement.innerHTML = data.usagetext;
-      document.querySelector("btn_all").textContent = data.btn_all;
+      document.querySelector(".usagetext1").textContent = data.usagetext1;
+      document.querySelector(".usagetext2").textContent = data.usagetext2;
+      document.querySelector(".btn_all").textContent = data.btn_all;
+      document.querySelector(".btn_lr").textContent = data.btn_lr;
+      document.querySelector(".btn_br1").textContent = data.btn_br1;
+      document.querySelector(".btn_br2").textContent = data.btn_br2;
+      document.querySelector(".btn_kitchen").textContent = data.btn_kitchen;
+      document.querySelector(".btn_corr").textContent = data.btn_corr;
       document.getElementById("englishButton").textContent =
         data.buttons.english;
       document.getElementById("finnishButton").textContent =
