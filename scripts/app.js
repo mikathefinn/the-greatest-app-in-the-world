26c79879cf0cb3826eea254eb7a06a15644db94d
mikathefinn <mikathefinn@gmail.com>
2023-10-09 09:05:35 +0300

    timer and temp functionalities, new pages, bottom navbar link fix

--- scripts/app.js
+++ scripts/app.js
@@ -10,13 +10,3 @@ const func = () => {
 
 func();
 setInterval(func, ms);
-
-const navItems = document.querySelectorAll('.mobile-bottom-nav__item');
-navItems.forEach(function (e, i) {
-  e.addEventListener('click', function (e) {
-    navItems.forEach(function (e2, i2) {
-      e2.classList.remove('mobile-bottom-nav__item--active');
-    });
-    this.classList.add('mobile-bottom-nav__item--active');
-  });
-});
