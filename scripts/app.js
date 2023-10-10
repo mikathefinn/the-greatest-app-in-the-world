79af0c97207e425e2e2238bdd4fb34df3ba4723a
mikathefinn <mikathefinn@gmail.com>
2023-10-10 11:35:40 +0300

    js to script file

--- scripts/app.js
+++ scripts/app.js
@@ -10,3 +10,41 @@ const func = () => {
 
 func();
 setInterval(func, ms);
+
+let currentTemp = 70;
+let timerInterval;
+let totalSeconds = 0;
+
+function changeTemperature(amount) {
+    currentTemp += amount;
+    document.getElementById('currentTemp').textContent = currentTemp +"˚c";
+}
+function startTimer() {
+  const timerInput = document.getElementById('timer').value.split(':');
+  const minutes = parseInt(timerInput[0]);
+  const seconds = parseInt(timerInput[1]);
+
+  if (!isNaN(minutes) && !isNaN(seconds) && (minutes > 0 || seconds > 0)) {
+      totalSeconds = minutes * 60 + seconds;
+      timerInterval = setInterval(updateTimer, 1000);
+  }
+}
+function updateTimer() {
+  const timerDisplay = document.getElementById('timerDisplay');
+  if (totalSeconds > 0) {
+      totalSeconds--;
+      const minutes = Math.floor(totalSeconds / 60);
+      const seconds = totalSeconds % 60;
+      timerDisplay.textContent = `Time remaining: ${minutes}:${seconds.toString().padStart(2, '0')}`;
+  } else {
+      clearInterval(timerInterval);
+      timerDisplay.textContent = 'Timer Expired';
+  }
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  totalSeconds = 0;
+  document.getElementById('timer').value = '';
+  document.getElementById('timerDisplay').textContent = '';
+}
